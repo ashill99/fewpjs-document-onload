3baaeaba3b495b097066bd38691edcc74111b19f
ashill99 <admshng@gmail.com>
2020-12-14 15:24:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    let textToChange = document.querySelector("p#text")
+    textToChange.textContent = "This is really cool!"
+  }); 
+
